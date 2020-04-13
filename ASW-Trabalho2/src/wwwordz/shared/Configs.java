b7d269e4d85d4f31fa495411cf88bfbfc5a39a17
up201706894 <edu.dado99@gamil.com>
2020-04-14 00:56:35 +0100

    Updated Configs to add alphabet

--- ASW-Trabalho2/src/wwwordz/shared/Configs.java
+++ ASW-Trabalho2/src/wwwordz/shared/Configs.java
@@ -5,6 +5,15 @@ public class Configs {
 	public static final long PLAY_STAGE_DURATION = 120000;
 	public static final long REPORT_STAGE_DURATION = 1000;
 	public static final long RANKING_STAGE_DURATION = 1000;
+	public static final int ALPHABET_SIZE = 26;
+	public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
 	
 	public Configs() {}
+	
+	public static int getAlphabetSize() {
+		return ALPHABET_SIZE;
+	}
+	public static String getAlphabet() {
+		return ALPHABET;
+	}
 }
