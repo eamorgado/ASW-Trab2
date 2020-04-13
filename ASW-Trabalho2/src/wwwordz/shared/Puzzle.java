aae1a305a3d8b60fa43566243d63c09fb66e00a5
up201706894 <edu.dado99@gamil.com>
2020-04-14 00:57:20 +0100

    Change layout of Puzzle class

--- ASW-Trabalho2/src/wwwordz/shared/Puzzle.java
+++ ASW-Trabalho2/src/wwwordz/shared/Puzzle.java
@@ -1,9 +1,11 @@
 package wwwordz.shared;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import wwwordz.shared.Table;
 import wwwordz.shared.Table.Cell;
+import wwwordz.shared.Puzzle.Solution;
 
 /**
  * Class representing the puzzle
@@ -14,39 +16,42 @@ import wwwordz.shared.Table.Cell;
  * 	are in neighboring cells on the grid and the letter in each cell is used only once
  * 
  * @author Eduardo Morgado (up201706894)
- * @author Ã‚ngelo Gomes (up201703990)
+ * @author Ângelo Gomes (up201703990)
  * @since April 2020
  */
 public class Puzzle implements Serializable{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private List<Puzzle.Solution> solutions;
+	private List<Solution> solutions;
 	private Table table;
 	
-	public static class Solution implements Serializable{
-		/**
-		 * 
-		 */
+	static class Solution implements Serializable{
 		private static final long serialVersionUID = 1L;
 		private List<Cell> cells;
 		private String word;
 		
-		public Solution(){}
+		public Solution(){
+			 cells = new ArrayList<>();;
+		}
+		
 		public Solution(String word, List<Cell> cells) {
 			this.word = word;
 			this.cells = cells;
 		}
 		
-		public String getWord() {return word;}
-		public List<Cell> getCells(){return cells;}
+		public String getWord() {
+			return word;
+		}
+		
+		public List<Cell> getCells(){
+			return cells;
+		}
 		
 		private int getPoints(int length) {
 			return length < 3? 0
 					: length == 3? 1
 					: 2 * getPoints(--length) + 1;
 		}
+		
 		public int getPoints() {
 			return getPoints(word.length());
 		}
@@ -56,8 +61,19 @@ public class Puzzle implements Serializable{
 		table = new Table();
 	}
 	
-	public Table getTable() {return table;}
-	public void setTable(Table table) {this.table = table;}
-	public List<Solution> getSolutions(){return solutions;}
-	public void setSolutions(List<Solution> solutions) {this.solutions = solutions;}
+	public Table getTable() {
+		return table;
+	}
+	
+	public void setTable(Table table) {
+		this.table = table;
+	}
+	
+	public List<Solution> getSolutions(){
+		return solutions;
+	}
+	
+	public void setSolutions(List<Solution> solutions) {
+		this.solutions = solutions;
+	}
 }
